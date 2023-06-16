1b1d7ccb129a3d539bb2c5b4ded7c7879aaaa2c9
Serhat <me.serhaterdem@gmail.com>
2023-06-16 22:48:24 +0300

    Architectural Changes-- Note: This section has not been completed. In virtual machine, app can generate some errors.

--- app/src/main/java/com/erdemserhat/ultimatebox/HomeFragment.java
+++ app/src/main/java/com/erdemserhat/ultimatebox/HomeFragment.java
@@ -1,6 +1,29 @@
 package com.erdemserhat.ultimatebox;
 
+import static android.content.Context.CLIPBOARD_SERVICE;
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.content.Context;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.erdemserhat.ultimatebox.random_password_generator.*;
+
+import android.content.ClipboardManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+import android.widget.Toast;
+
+import com.erdemserhat.ultimatebox.databinding.ActivityMainBinding;
 
 import androidx.fragment.app.Fragment;
 
@@ -8,12 +31,51 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.erdemserhat.ultimatebox.databinding.ActivityMainBinding;
+import com.erdemserhat.ultimatebox.databinding.FragmentHomeBinding;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link HomeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
+    private AlphaAnimation buttonClick = new AlphaAnimation(1F, 0.8F);
+
+    private FragmentHomeBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+
+    @Override
+    public void onViewCreated(View view,  Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
+
+        // Burada binding nesnesini kullanarak layout içindeki view'lara erişebilirsiniz
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Butona tıklandığında yapılacak işlemler
+                generate(v);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        // Fragment view'i yok edildiğinde binding nesnesini null'a ayarla
+        //binding = null;
+    }
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -53,12 +115,70 @@ public class HomeFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        /**
+         * Assigning the default config.
+         */
+
+
+
+        /**
+         * When user long clicks the password area, the current text of warning view will be copied to clipboard
+         */
+
+        binding.warning.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                String password = binding.warning.getText().toString();
+                ClipboardManager clipboardManager = (ClipboardManager) requireContext().getSystemService(Context.CLIPBOARD_SERVICE);
+                clipboardManager.setText(password);
+                Toast.makeText(requireContext(), R.string.infoCopiedText, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
+
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+    public void generate(View view) {
+        //Animation
+        view.startAnimation(buttonClick);
+        boolean isCharacter = binding.characterSwitch.isChecked();
+        boolean isNumerical = binding.numberSwitch.isChecked();
+        boolean isSpecialCharacter = binding.specialSwitch.isChecked();
+        String pwLengthTemp = binding.pwLenght.getText().toString();
+        int passwordLength;
+        if (pwLengthTemp.isEmpty()) {
+            passwordLength = 32;
+        } else {
+            passwordLength = Integer.parseInt(pwLengthTemp);
+        }
+
+        if (isCharacter || isSpecialCharacter || isNumerical) {
+
+            if (passwordLength < 4) {
+                binding.warning.setText(R.string.infoDigitLess);
+                return;
+            } else if (passwordLength > 10000) {
+                binding.warning.setText(R.string.infoDigitMore);
+                return;
+
+            }
+            Generator generator = new Generator(isNumerical, isCharacter, isSpecialCharacter, passwordLength);
+            String password = generator.createPassword();
+            binding.pwTitle.setVisibility(View.VISIBLE);
+            binding.warning.setText(password);
+
+
+            ClipboardManager clipboardManager = (ClipboardManager) requireContext().getSystemService(Context.CLIPBOARD_SERVICE);
+            clipboardManager.setText(password);
+            Toast.makeText(HomeFragment.this.getContext(), R.string.infoCopiedText, Toast.LENGTH_SHORT).show();
+        } else {
+
+            binding.warning.setText(R.string.infoRadioButton);
+        }
+
     }
+
+
 }
--- app/src/main/java/com/erdemserhat/ultimatebox/MainActivity.java
+++ app/src/main/java/com/erdemserhat/ultimatebox/MainActivity.java
@@ -5,20 +5,14 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.erdemserhat.ultimatebox.random_password_generator.*;
-import android.content.ClipboardManager;
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.AlphaAnimation;
-import android.widget.Toast;
+
 import com.erdemserhat.ultimatebox.databinding.ActivityMainBinding;
 
 
-
-public class   MainActivity extends AppCompatActivity {
-
-    private AlphaAnimation buttonClick = new AlphaAnimation(1F, 0.8F);
-    /**View binding data member assignment*/
+public class MainActivity extends AppCompatActivity {
+    //View binding data member assignment
     private ActivityMainBinding binding;
 
     @Override
@@ -30,37 +24,15 @@ public class   MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
-        /**
-         * Assigning the default config.
-         */
-        binding.numberSwitch.setChecked(true);
-        binding.characterSwitch.setChecked(true);
-        binding.specialSwitch.setChecked(true);
-        changeFragment(new HomeFragment());
+        //changeFragment(new HomeFragment());
 
 
-        /**
-         * When user long clicks the password area, the current text of warning view will be copied to clipboard
-         */
-
-        binding.warning.setOnLongClickListener(new View.OnLongClickListener() {
-
-            @Override
-            public boolean onLongClick(View view) {
-                String password = binding.warning.getText().toString();
-                ClipboardManager clipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
-                clipboardManager.setText(password);
-                Toast.makeText(MainActivity.this, R.string.infoCopiedText, Toast.LENGTH_SHORT).show();
-                return true;
-
-            }
-        });
         /**
          * SetOnClickListener for Navigation Menu....
          */
 
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
-            switch (item.getItemId()){
+            switch (item.getItemId()) {
 
                 case R.id.history:
                     //When history icon is clicked on the nav bar.
@@ -86,69 +58,22 @@ public class   MainActivity extends AppCompatActivity {
 
 
             }
-
-
-
-
-            return  true;
+            return true;
         });
 
 
-
     }
 
-    public void generate(View view){
-        //Animation
-        view.startAnimation(buttonClick);
-        boolean isCharacter=binding.characterSwitch.isChecked();
-        boolean isNumerical =binding.numberSwitch.isChecked();
-        boolean isSpecialCharacter=binding.specialSwitch.isChecked();
-        String pwLengthTemp = binding.pwLenght.getText().toString();
-        int passwordLength;
-        if(pwLengthTemp.isEmpty()) {
-            passwordLength=32;
-        }else{
-            passwordLength = Integer.parseInt(pwLengthTemp);
-        }
-
-        if(isCharacter || isSpecialCharacter || isNumerical){
-
-            if(passwordLength<4){
-                binding.warning.setText(R.string.infoDigitLess);
-                return;
-            }else if(passwordLength>10000){
-                binding.warning.setText(R.string.infoDigitMore);
-                return;
-
-            }
-            Generator generator = new Generator(isNumerical,isCharacter,isSpecialCharacter,passwordLength);
-            String password = generator.createPassword();
-            binding.pwTitle.setVisibility(View.VISIBLE);
-            binding.warning.setText(password);
-
-
-            ClipboardManager clipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
-            clipboardManager.setText(password);
-            Toast.makeText(this, R.string.infoCopiedText, Toast.LENGTH_SHORT).show();
-        }else{
-
-            binding.warning.setText(R.string.infoRadioButton);
-        }
-
-        }
     /**
      * This method changes the current fragment with the fragment which comes as parameter
      */
-    private void changeFragment(Fragment fragment){
+    private void changeFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
         fragmentTransaction.commit();
 
     }
-
-
-
 
 
 }
